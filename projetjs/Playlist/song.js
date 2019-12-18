7ee3bc689d7d39573c4989d3738ce2701c93cee0
ApoSkunz <alexandresolane.web@gmail.com>
2019-12-18 15:29:12 +0100

    Modification path + constructeur media

---
+++ projetjs/Playlist/song.js
@@ -0,0 +1,41 @@
+// Création du constructeur media
+
+function Media(title,duration,isPlaying)
+{
+	this.title = title;
+	this.duration = duration;
+	this.isPlaying = isPlaying;
+
+	this.play = function()
+	{
+		isPlaying = true;
+		// Lire le fichier audio ou vidéo
+		// Passer isPlaying à True
+
+	}
+
+	this.stop = function()
+	{
+		// Stopper le fichier audio ou vidéo
+		// Passer isPlaying à False
+	}
+}
+
+function Song(title,duration,isPlaying,artist)
+{
+	Media.call(this,title,duration,isPlaying);
+
+	this.artist = artist;
+}
+
+Song.prototype = Object.create(Media.prototype);
+
+function Movie(title,duration,isPlaying,year)
+{
+	Media.call(this,title,duration,isPlaying);
+
+	this.year = year;
+}
+
+Movie.prototype = Object.create(Media.prototype);
+
