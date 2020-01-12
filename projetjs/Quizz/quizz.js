eb4d2b3765fffc18718e6f81b89dfc89a594816f
HugoVeron <hugo.veron@free.fr>
2020-01-12 15:34:54 +0100

    bon compte des points

--- projetjs/Quizz/quizz.js
+++ projetjs/Quizz/quizz.js
@@ -7,7 +7,7 @@ function entierAleatoire(min, max)
 document.getElementById('QCM').style.visibility='hidden';
 document.getElementById('libre').style.visibility='hidden';
 document.getElementById('valider').style.visibility='hidden';
-document.getElementById('bonnes_reponses').style.visibility='hidden';
+//document.getElementById('bonnes_reponses').style.visibility='hidden';
 document.getElementById("confirmation_connexion").style.visibility="hidden"
 
 let audio;// Création de la balise pour jouer les musiqques lors du blind test
@@ -21,27 +21,24 @@ while(mauvaise_rep==num_playlist || list_of_media[mauvaise_rep][4]==list_of_medi
 {
 	mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
 }
-let mauvaise_annee //on crée la variable mauvaise année au cas ou tous les medias datent de la meme année et que l'on doivent changer l'année
+let mauvaise_annee=list_of_media[mauvaise_rep][5];//on crée la variable mauvaise année au cas ou tous les medias datent de la meme année et que l'on doivent changer l'année
 let bonne_option = entierAleatoire(1,2);// permet de déterminer la place de la bonne option 
 let type_question = entierAleatoire(1,2);//permet de déterminer le type de question que l'on va poser si le media est de type "song"
 let type_media = list_of_media[num_playlist][0]; //on prend le type de media pour determiner quelles question on va poser 
 document.getElementById("commencer").addEventListener('click', () => {//on commence le quizz quand on appuie sur le bouton "commencer le quizz"
 	document.getElementById('valider').style.visibility='initial';//on affiche le bouton "valider"
+	document.getElementById('commencer').style.visibility='hidden';
 	if (type_media == "video")//on pose la question relative à la vidéo(film)
 	{
 		if (list_of_media[mauvaise_rep][5] == list_of_media[num_playlist][5])// si les deux réponses sont les memes
 		{
-			mauvaise_annee=entierAleatoire(list_of_media[num_playlist][5]-3,list_of_media[num_playlist][5]);
+			mauvaise_annee=entierAleatoire(list_of_media[num_playlist][5]-3,list_of_media[num_playlist][5]-1);
 		}
 		document.getElementById("question").innerText="donnez l'annee de sortie de la video : " + list_of_media[num_playlist][1]; //on affiche la bonne question 
 		if(bonne_option==1)//choix pour placer les checkboxs
 		{
 			document.getElementById("option1").innerText=list_of_media[num_playlist][5];//on affiche les bonnes checkboxs
 			document.getElementById("option2").innerText=mauvaise_annee;
-			if(document.getElementById("checkbox1").checked == true)//on actualise le score
-			{
-				bonnes_rep+=1
-			}
 			document.getElementById("checkbox1").checked =false;// on remet les check box à 0
 			document.getElementById("checkbox2").checked =false;
 		}
@@ -49,23 +46,9 @@ document.getElementById("commencer").addEventListener('click', () => {//on comme
 		{
 			document.getElementById("option1").innerText=mauvaise_annee;
 			document.getElementById("option2").innerText=list_of_media[num_playlist][5];
-			if(document.getElementById("checkbox2").checked == true)
-			{
-				bonnes_rep+=1
-			}
 			document.getElementById("checkbox1").checked =false;
 			document.getElementById("checkbox2").checked =false;
 		}
-		//on regénère toutes les données àléatoires
-		num_playlist = entierAleatoire(0,list_of_media.length-1);
-		mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-		while(mauvaise_rep==num_playlist)
-		{
-			mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-		}
-		lbonne_option = entierAleatoire(1,2);
-		type_question = entierAleatoire(1,2);
-		type_media = list_of_media[num_playlist][0];
 		document.getElementById('QCM').style.visibility='initial';//on affiche les bons éléments
 		document.getElementById('libre').style.visibility='hidden';
 	}
@@ -78,10 +61,6 @@ document.getElementById("commencer").addEventListener('click', () => {//on comme
 			{
 				document.getElementById("option1").innerText=list_of_media[num_playlist][4];
 				document.getElementById("option2").innerText=list_of_media[mauvaise_rep][4];
-				if(document.getElementById("checkbox1").checked == true)
-				{
-					bonnes_rep+=1
-				}
 				document.getElementById("checkbox1").checked =false;
 				document.getElementById("checkbox2").checked =false;
 
@@ -90,22 +69,9 @@ document.getElementById("commencer").addEventListener('click', () => {//on comme
 			{
 				document.getElementById("option1").innerText=list_of_media[mauvaise_rep][4];
 				document.getElementById("option2").innerText=list_of_media[num_playlist][4];
-				if(document.getElementById("checkbox2").checked == true)
-				{
-					bonnes_rep+=1
-				}
 				document.getElementById("checkbox1").checked =false;
 				document.getElementById("checkbox2").checked =false;
 			}
-			num_playlist = entierAleatoire(0,list_of_media.length-1);
-			mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-			while(mauvaise_rep==num_playlist)
-			{
-				mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-			}
-			lbonne_option = entierAleatoire(1,2);
-			type_question = entierAleatoire(1,2);
-			type_media = list_of_media[num_playlist][0];
 			document.getElementById('QCM').style.visibility='initial';
 			document.getElementById('libre').style.visibility='hidden';
 
@@ -120,10 +86,19 @@ document.getElementById("commencer").addEventListener('click', () => {//on comme
 			num_playlist = entierAleatoire(0,list_of_media.length-1);
 			mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
 			texte_input = document.getElementById("titre_input").value;
-			if(document.getElementById("titre_input").value == list_of_media[num_playlist][1] )
+			document.getElementById("valider").addEventListener('click', () => 
+			{
+				if (type_media == "song" && nb_question==1 && type_question == 2)
 				{
-					bonnes_rep+=1
+					if(document.getElementById("titre_input").value.toString() == list_of_media[num_playlist][1].toString )
+					{
+						bonnes_rep+=1;
+						document.getElementById('bonnes_reponses').innerText="pour_l'incrémentation"+bonnes_rep;
+						document.getElementById('bonnes_reponses').innerText=document.getElementById("titre_input").value;
+					}
+					document.getElementById('bonnes_reponses').innerText=(document.getElementById("titre_input").value.toString() == list_of_media[num_playlist][1].toString );
 				}
+			});
 			document.getElementById("titre_input").value = "";
 			bonne_option = entierAleatoire(1,2);
 			type_question = entierAleatoire(1,2);
@@ -133,30 +108,38 @@ document.getElementById("commencer").addEventListener('click', () => {//on comme
 		}
 	}
 });
-document.getElementById("valider").addEventListener('click', () => {
+document.getElementById("next").addEventListener('click', () => {
 	//cette fonction est censée arréter la musique quand on clique sur le bouton envoyer, si une musique à été joué, cependant l'id attribué à la balise n'est plus reconnue, la commande ne marche donc pas
 	/*if(music_is_playing)
 	{
 		document.getElementById("audio").innerHTML='';
 		music_is_playing = false;
 	}*/
+	//on regénère toutes les données aléatoires
+	num_playlist = entierAleatoire(0,list_of_media.length-1);
+	mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
+	while(mauvaise_rep==num_playlist)
+	{
+		mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
+	}
+	lbonne_option = entierAleatoire(1,2);
+	type_question = entierAleatoire(1,2);
+	type_media = list_of_media[num_playlist][0];
+	document.getElementById('bonnes_reponses').innerText="pasteque";
+	nb_question+=1;
 	if (nb_question<list_of_media.length+1)//on vérifie que le nombre de question ne dépasse pas la taille de la playlist( je n'ai pas eu le temps de faire en sorte )
 	{
 		if (type_media == "video")
 		{
 			if (list_of_media[mauvaise_rep][5] == list_of_media[num_playlist][5])
 			{
-				mauvaise_annee=entierAleatoire(list_of_media[num_playlist][5]-3,list_of_media[num_playlist][5]);
+				mauvaise_annee=entierAleatoire(list_of_media[num_playlist][5]-3,list_of_media[num_playlist][5]-1);
 			}
 			document.getElementById("question").innerText="donnez l'annee de sortie de la vidéo : " + list_of_media[num_playlist][1]; 
 			if (bonne_option==1)
 			{
 				document.getElementById("option1").innerText=list_of_media[num_playlist][5];
 				document.getElementById("option2").innerText=mauvaise_annee;
-				if(document.getElementById("checkbox1").checked == true)
-				{
-					bonnes_rep+=1
-				}
 				document.getElementById("checkbox1").checked =false;
 				document.getElementById("checkbox2").checked =false;
 			}
@@ -164,22 +147,9 @@ document.getElementById("valider").addEventListener('click', () => {
 			{
 				document.getElementById("option1").innerText=mauvaise_annee;
 				document.getElementById("option2").innerText=list_of_media[num_playlist][5];
-				if(document.getElementById("checkbox2").checked == true)
-				{
-					bonnes_rep+=1
-				}
 				document.getElementById("checkbox1").checked =false;
 				document.getElementById("checkbox2").checked =false;
 			}
-			num_playlist = entierAleatoire(0,list_of_media.length-1);
-			mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-			while(mauvaise_rep==num_playlist)
-			{
-				mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-			}
-			bonne_option = entierAleatoire(1,2);
-			type_question = entierAleatoire(1,2);
-			type_media = list_of_media[num_playlist][0];
 			document.getElementById('QCM').style.visibility='initial';
 			document.getElementById('libre').style.visibility='hidden';
 		}
@@ -192,33 +162,16 @@ document.getElementById("valider").addEventListener('click', () => {
 				{
 					document.getElementById("option1").innerText=list_of_media[num_playlist][4];
 					document.getElementById("option2").innerText=list_of_media[mauvaise_rep][4];
-					if(document.getElementById("checkbox1").checked == true)
-					{
-						bonnes_rep+=1
-					}
 					document.getElementById("checkbox1").checked =false;
 					document.getElementById("checkbox2").checked =false;
 				}
 				else
 				{
-					document.getElementById("option2").innerText=list_of_media[mauvaise_rep][4];
-					document.getElementById("option1").innerText=list_of_media[num_playlist][4];
-					if(document.getElementById("checkbox2").checked == true)
-					{
-						bonnes_rep+=1
-					}
+					document.getElementById("option1").innerText=list_of_media[mauvaise_rep][4];
+					document.getElementById("option2").innerText=list_of_media[num_playlist][4];
 					document.getElementById("checkbox1").checked =false;
 					document.getElementById("checkbox2").checked =false;
 				}
-				num_playlist = entierAleatoire(0,list_of_media.length-1);
-				mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-				while(mauvaise_rep==num_playlist)
-				{
-					mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-				}
-				bonne_option = entierAleatoire(1,2);
-				type_question = entierAleatoire(1,2);
-				type_media = list_of_media[num_playlist][0];
 				document.getElementById('QCM').style.visibility='initial';
 				document.getElementById('libre').style.visibility='hidden';
 			}
@@ -230,27 +183,10 @@ document.getElementById("valider").addEventListener('click', () => {
 				audio.play();
 				music_is_playing = true;
 				document.getElementById("titre").innerText="rentrez le titre de la chanson";
-				num_playlist = entierAleatoire(0,list_of_media.length-1);
-				mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-				while(mauvaise_rep==num_playlist)
-				{
-					mauvaise_rep = entierAleatoire(0,list_of_media.length-1);
-				}
-				texte_input = document.getElementById("titre_input").value;
-				if(document.getElementById("titre_input").value == list_of_media[num_playlist][1] )
-				{
-					bonnes_rep+=1
-					document.getElementById('bonnes_reponses').innerText="pasteque";
-				}
-				document.getElementById("titre_input").value = "";
-				bonne_option = entierAleatoire(1,2);
-				type_question = entierAleatoire(1,2);
-				type_media = list_of_media[num_playlist][0];
 				document.getElementById('libre').style.visibility='initial';
 				document.getElementById('QCM').style.visibility='hidden';
 			}
 		}
-		nb_question+=1;// on augmente nombre de question pour vérifier que l'on en fait pas trop 
 	}
 	else 
 	{
@@ -285,4 +221,50 @@ document.getElementById("formi8").addEventListener('click', () =>
 	document.getElementById('Surname').value='';
 	document.getElementById('Name').value='';
 	document.getElementById('email').value='';
+});
+
+document.getElementById("valider").addEventListener('click', () => 
+{
+	if (type_media == "video" && bonne_option==1)
+	{
+		if(document.getElementById("checkbox1").checked)//on actualise le score
+		{
+			bonnes_rep+=1;
+			document.getElementById('bonnes_reponses').innerText="pour_l'incrémentation"+bonnes_rep;
+		}				
+	}
+	else if (type_media == "video" && bonne_option==2)
+	{
+		if(document.getElementById("checkbox2").checked)//on actualise le score
+		{
+			bonnes_rep+=1;
+			document.getElementById('bonnes_reponses').innerText="pour_l'incrémentation"+bonnes_rep;
+		}				
+	}
+	else if (type_media == "song" && bonne_option==1 && type_question==1)
+	{
+		if(document.getElementById("checkbox1").checked)//on actualise le score
+		{
+			bonnes_rep+=1;
+			document.getElementById('bonnes_reponses').innerText="pour_l'incrémentation"+bonnes_rep;
+		}				
+	}
+	else if (type_media == "song" && bonne_option==2 && type_question==1)
+	{
+		if(document.getElementById("checkbox2").checked)//on actualise le score
+		{
+			bonnes_rep+=1;
+			document.getElementById('bonnes_reponses').innerText="pour_l'incrémentation"+bonnes_rep;
+		}				
+		
+	}
+	if (type_media == "song" && type_question == 2)
+	{
+		if(document.getElementById("titre_input").value.toString() == list_of_media[num_playlist][1] )
+		{
+			bonnes_rep+=1;
+			document.getElementById('bonnes_reponses').innerText="pour_l'incrémentation"+bonnes_rep;
+		}
+		document.getElementById('bonnes_reponses').innerText=document.getElementById("titre_input").value.toString() == list_of_media[num_playlist][1];
+	}
 });
